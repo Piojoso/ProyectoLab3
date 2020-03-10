1f4940fdc89cc9b5fdb1e4e3109d02f0a5b6a29f
Leonel Bongiovanni <labongiovanni@live.com.ar>
2020-03-10 13:36:47 -0300

    Nuevo user control creado ucDGVTabla
    Este user control sera usado en diferentes forms para enseñar la
    informacion deseada en un DGV.
    
    Actualmente esta la toda la funcionabilidad principal. Aun hay trabajo
    por hacerle.

---
+++ ucLibrary/ucDGVTabla.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace ucLibrary
+{
+    partial class ucDGVTabla
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.pnlBG = new System.Windows.Forms.Panel();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.dgvPrincipal = new System.Windows.Forms.DataGridView();
+            this.pnlAside = new System.Windows.Forms.Panel();
+            this.gbButtons = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new FontAwesome.Sharp.IconButton();
+            this.btnSearch = new FontAwesome.Sharp.IconButton();
+            this.btnSeeAll = new FontAwesome.Sharp.IconButton();
+            this.btnModify = new FontAwesome.Sharp.IconButton();
+            this.btnAdd = new FontAwesome.Sharp.IconButton();
+            this.pnlBG.SuspendLayout();
+            this.pnlContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrincipal)).BeginInit();
+            this.pnlAside.SuspendLayout();
+            this.gbButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBG
+            // 
+            this.pnlBG.Controls.Add(this.pnlContent);
+            this.pnlBG.Controls.Add(this.pnlAside);
+            this.pnlBG.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBG.Location = new System.Drawing.Point(0, 0);
+            this.pnlBG.Name = "pnlBG";
+            this.pnlBG.Size = new System.Drawing.Size(886, 443);
+            this.pnlBG.TabIndex = 0;
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Controls.Add(this.dgvPrincipal);
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(0, 0);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Padding = new System.Windows.Forms.Padding(5);
+            this.pnlContent.Size = new System.Drawing.Size(686, 443);
+            this.pnlContent.TabIndex = 1;
+            // 
+            // dgvPrincipal
+            // 
+            this.dgvPrincipal.AllowUserToAddRows = false;
+            this.dgvPrincipal.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.NullValue = null;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            this.dgvPrincipal.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvPrincipal.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPrincipal.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvPrincipal.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvPrincipal.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPrincipal.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.dgvPrincipal.ColumnHeadersHeight = 35;
+            this.dgvPrincipal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvPrincipal.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dgvPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPrincipal.EnableHeadersVisualStyles = false;
+            this.dgvPrincipal.Location = new System.Drawing.Point(5, 5);
+            this.dgvPrincipal.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvPrincipal.MultiSelect = false;
+            this.dgvPrincipal.Name = "dgvPrincipal";
+            this.dgvPrincipal.ReadOnly = true;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPrincipal.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.dgvPrincipal.RowHeadersVisible = false;
+            this.dgvPrincipal.RowHeadersWidth = 10;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            this.dgvPrincipal.RowsDefaultCellStyle = dataGridViewCellStyle10;
+            this.dgvPrincipal.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPrincipal.Size = new System.Drawing.Size(676, 433);
+            this.dgvPrincipal.TabIndex = 6;
+            // 
+            // pnlAside
+            // 
+            this.pnlAside.Controls.Add(this.gbButtons);
+            this.pnlAside.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlAside.Location = new System.Drawing.Point(686, 0);
+            this.pnlAside.Name = "pnlAside";
+            this.pnlAside.Padding = new System.Windows.Forms.Padding(10, 5, 10, 10);
+            this.pnlAside.Size = new System.Drawing.Size(200, 443);
+            this.pnlAside.TabIndex = 3;
+            // 
+            // gbButtons
+            // 
+            this.gbButtons.Controls.Add(this.btnDelete);
+            this.gbButtons.Controls.Add(this.btnSearch);
+            this.gbButtons.Controls.Add(this.btnSeeAll);
+            this.gbButtons.Controls.Add(this.btnModify);
+            this.gbButtons.Controls.Add(this.btnAdd);
+            this.gbButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbButtons.Location = new System.Drawing.Point(10, 5);
+            this.gbButtons.Name = "gbButtons";
+            this.gbButtons.Size = new System.Drawing.Size(180, 428);
+            this.gbButtons.TabIndex = 2;
+            this.gbButtons.TabStop = false;
+            this.gbButtons.Text = "Acciones";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.IndianRed;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnDelete.IconChar = FontAwesome.Sharp.IconChar.Trash;
+            this.btnDelete.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDelete.IconSize = 25;
+            this.btnDelete.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnDelete.Location = new System.Drawing.Point(6, 206);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Rotation = 0D;
+            this.btnDelete.Size = new System.Drawing.Size(168, 35);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnSearch.IconChar = FontAwesome.Sharp.IconChar.Search;
+            this.btnSearch.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSearch.IconSize = 25;
+            this.btnSearch.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSearch.Location = new System.Drawing.Point(6, 165);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Rotation = 0D;
+            this.btnSearch.Size = new System.Drawing.Size(168, 35);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnSeeAll
+            // 
+            this.btnSeeAll.BackColor = System.Drawing.Color.DarkGray;
+            this.btnSeeAll.FlatAppearance.BorderSize = 0;
+            this.btnSeeAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSeeAll.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnSeeAll.IconChar = FontAwesome.Sharp.IconChar.List;
+            this.btnSeeAll.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSeeAll.IconSize = 25;
+            this.btnSeeAll.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSeeAll.Location = new System.Drawing.Point(6, 124);
+            this.btnSeeAll.Name = "btnSeeAll";
+            this.btnSeeAll.Rotation = 0D;
+            this.btnSeeAll.Size = new System.Drawing.Size(168, 35);
+            this.btnSeeAll.TabIndex = 2;
+            this.btnSeeAll.UseVisualStyleBackColor = false;
+            this.btnSeeAll.Click += new System.EventHandler(this.btnSeeAll_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnModify.FlatAppearance.BorderSize = 0;
+            this.btnModify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModify.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnModify.IconChar = FontAwesome.Sharp.IconChar.Edit;
+            this.btnModify.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.btnModify.IconSize = 25;
+            this.btnModify.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnModify.Location = new System.Drawing.Point(6, 83);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Rotation = 0D;
+            this.btnModify.Size = new System.Drawing.Size(168, 35);
+            this.btnModify.TabIndex = 1;
+            this.btnModify.UseVisualStyleBackColor = false;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.ForestGreen;
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnAdd.IconChar = FontAwesome.Sharp.IconChar.Plus;
+            this.btnAdd.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.btnAdd.IconSize = 25;
+            this.btnAdd.Location = new System.Drawing.Point(6, 42);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Rotation = 0D;
+            this.btnAdd.Size = new System.Drawing.Size(168, 35);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // ucDGVTabla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlBG);
+            this.Name = "ucDGVTabla";
+            this.Size = new System.Drawing.Size(886, 443);
+            this.pnlBG.ResumeLayout(false);
+            this.pnlContent.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrincipal)).EndInit();
+            this.pnlAside.ResumeLayout(false);
+            this.gbButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBG;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.GroupBox gbButtons;
+        private System.Windows.Forms.Panel pnlAside;
+        private System.Windows.Forms.DataGridView dgvPrincipal;
+        private FontAwesome.Sharp.IconButton btnDelete;
+        private FontAwesome.Sharp.IconButton btnSearch;
+        private FontAwesome.Sharp.IconButton btnSeeAll;
+        private FontAwesome.Sharp.IconButton btnModify;
+        private FontAwesome.Sharp.IconButton btnAdd;
+    }
+}
---
+++ ucLibrary/ucDGVTabla.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+namespace ucLibrary
+{
+    public partial class ucDGVTabla : UserControl
+    {
+        public ucDGVTabla()
+        {
+
+            /**
+             * ----------------------------------------------------------------------------------------------------------------------
+             * TODO: Funcion de botones add, modify, see all, search, delete en este codigo, y tratar de que sea generico para todo.
+             * --- Hecho: Masomenos, en realidad lo que luego se me ocurrio fue hacer que cada boton habra una correspondiente ventana,
+             * ---        y ahi se realiza lo que se desee. Agregar, modificar, etc etc.
+             * 
+             * TODO: De alguna manera debo hacer que se pueda seleccionar que columnas se quiere enseñar
+             * 
+             * TODO: Hacer que se pueda cambiar el nombre de las columnas en el DGV
+             * 
+             * TODO: Hacer que se puedan ocultar columnas en el DGV
+             * 
+             * NOTA: Provablemente para todo esto haga falta hacer uso de reflexion y linq y quien sabe que dios mas.
+             * ----------------------------------------------------------------------------------------------------------------------
+             */
+            InitializeComponent();
+        }
+
+        #region Colors of Button
+
+        private Color fondoBotones;
+        
+        public Color FondoBotones
+        {
+            get { return fondoBotones; }
+            set
+            {
+                fondoBotones = value;
+
+                if (!fondoBotones.IsEmpty)
+                {
+                    foreach (IconButton btn in gbButtons.Controls)
+                    {
+                        btn.BackColor = fondoBotones;
+                    }
+                }
+                else
+                {
+                    btnAdd.BackColor = colorsButtons.btnAdd;
+                    btnModify.BackColor = colorsButtons.btnModify;
+                    btnSeeAll.BackColor = colorsButtons.btnSeeAll;
+                    btnSearch.BackColor = colorsButtons.btnSearch;
+                    btnDelete.BackColor = colorsButtons.btnDelete;
+                }
+            }
+        }
+
+        private struct colorsButtons
+        {
+            public static Color btnAdd = Color.ForestGreen;
+            public static Color btnModify = Color.FromArgb(192, 192, 0);
+            public static Color btnSeeAll = Color.DarkGray;
+            public static Color btnSearch = Color.SteelBlue;
+            public static Color btnDelete = Color.IndianRed;
+        }
+
+        public Color buttonAdd
+        {
+            get { return colorsButtons.btnAdd; }
+            set { colorsButtons.btnAdd = value; }
+        }
+
+        public Color buttonModify
+        {
+            get { return colorsButtons.btnModify; }
+            set { colorsButtons.btnModify = value; }
+        }
+
+        public Color buttonSeeAll
+        {
+            get { return colorsButtons.btnSeeAll; }
+            set { colorsButtons.btnSeeAll = value; }
+        }
+
+        public Color buttonSearch
+        {
+            get { return colorsButtons.btnSearch; }
+            set { colorsButtons.btnSearch = value; }
+        }
+
+        public Color buttonDelete
+        {
+            get { return colorsButtons.btnDelete; }
+            set { colorsButtons.btnDelete = value; }
+        }
+
+        #endregion
+
+        #region DGVStyle
+        
+        private DataGridViewCellStyle columnHeaders;
+        private DataGridViewCellStyle rowsHeaders;
+        private DataGridViewCellStyle rowsCells;
+
+        public DataGridViewCellStyle ColumnHeaderStyle
+        {
+            get { return columnHeaders; }
+            set
+            {
+                columnHeaders = value;
+
+                if(columnHeaders != null)
+                    dgvPrincipal.ColumnHeadersDefaultCellStyle = columnHeaders;
+            }
+        }
+
+        public DataGridViewCellStyle RowsHeaderStyle
+        {
+            get { return rowsHeaders; }
+            set
+            {
+                rowsHeaders = value;
+
+                if (columnHeaders != null)
+                    dgvPrincipal.RowHeadersDefaultCellStyle = rowsHeaders;
+            }
+        }
+
+        public DataGridViewCellStyle RowsCellStyle
+        {
+            get { return rowsCells; }
+            set
+            {
+                rowsCells = value;
+
+                if (columnHeaders != null)
+                    dgvPrincipal.DefaultCellStyle = rowsCells;
+            }
+        }
+
+        #endregion
+
+        #region Botones y sus Forms
+
+        private struct forms
+        {
+            public static Form frmAdd;
+            public static Form frmModify;
+            public static Form frmSeeAll;
+            public static Form frmSearch;
+            public static Form frmDelete;
+        }
+
+        public Form FrmAdd
+        {
+            get { return forms.frmAdd; }
+            set { forms.frmAdd = value; }
+        }
+
+        public Form FrmModify
+        {
+            get { return forms.frmModify; }
+            set { forms.frmModify = value; }
+        }
+
+        public Form FrmSeeAll
+        {
+            get { return forms.frmSeeAll; }
+            set { forms.frmSeeAll = value; }
+        }
+
+        public Form FrmSearch
+        {
+            get { return forms.frmSearch; }
+            set { forms.frmSearch = value; }
+        }
+
+        public Form FrmDelete
+        {
+            get { return forms.frmDelete; }
+            set { forms.frmDelete = value; }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            forms.frmAdd?.ShowDialog();
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            forms.frmModify?.ShowDialog();
+        }
+
+        private void btnSeeAll_Click(object sender, EventArgs e)
+        {
+            forms.frmSeeAll?.ShowDialog();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            forms.frmSearch?.ShowDialog();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            forms.frmDelete?.ShowDialog();
+        }
+
+        #endregion
+
+        public void dataSource(object data)
+        {
+            dgvPrincipal.DataSource = data;
+        }
+        
+    }
+}
