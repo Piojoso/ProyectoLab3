aadf3f2e9de51c587e79a00c98596418f0b049e9
Leonel Bongiovanni <labongiovanni@live.com.ar>
2020-04-07 19:36:48 -0300

    Nuevo frmSeleccionarCliente agregado
    Este formulario permitira hacer una seleccion sobre todos los
    clientes, o incluso realizar una busqueda para achicar la lista y asi
    obtener el id del cliente que estamos buscando para poder proceder con
    otras acciones que requieran un Cliente en particular.

---
+++ MAB/Forms/Clientes/frmSeleccionarCliente.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MAB.Models;
+using MAB.Forms.CRUD.Clientes;
+
+namespace MAB.Forms.Clientes
+{
+    public partial class frmSeleccionarCliente : Form
+    {
+        public frmSeleccionarCliente()
+        {
+            /**
+             * TODO: revisar el correcto funcionamiento
+             */
+            InitializeComponent();
+
+            ucBottom.Accion1 = "Seleccionar";
+            ucBottom.Accion2 = "Buscar";
+            ucBottom.Accion3 = "Cerrar";
+
+            ucBottom.evAccion1 += seleccionarCliente;
+            ucBottom.evAccion2 += buscarCliente;
+            ucBottom.evAccion3 += cerrarVentana;
+
+            cargarLista();
+        }
+
+        private int idCliente = -1;
+
+        public int ClienteSeleccionado
+        {
+            get { return idCliente; }
+        }
+
+        private void cargarLista()
+        {
+            using (MABEntities db = new MABEntities())
+            {
+                ucDGVTabla.dataSource(db.Clientes.ToList());
+            }
+
+            ucDGVTabla.Columns["Id"].Visible = false;
+            ucDGVTabla.Columns["Telefonos"].Visible = false;
+            ucDGVTabla.Columns["Lavarropas"].Visible = false;
+            ucDGVTabla.Columns["Entregas"].Visible = false;
+        }
+
+        #region Eventos de ucBottom
+
+        private void seleccionarCliente(object sender, EventArgs e)
+        {
+            if(ucDGVTabla.selectedRow() != null)
+            {
+                using (MABEntities db = new MABEntities())
+                {
+                    Models.Clientes cliente = db.Clientes.Find(Convert.ToInt32(ucDGVTabla.selectedRow().Cells["Id"].Value));
+
+                    DialogResult resp = MessageBox.Show(
+                        "Ha seleccionado al Cliente: \n" +
+                        cliente.nombre + " " + cliente.apellido + "\n" +
+                        "¿Desea Continuar?", "Atencion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                    if(resp == DialogResult.Yes)
+                    {
+                        idCliente = cliente.Id;
+                        this.Close();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No hay una fila seleccionada", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buscarCliente(object sender, EventArgs e)
+        {
+            frmBuscarCliente frm = new frmBuscarCliente();
+            frm.ShowDialog();
+
+            if(frm.idClientes != null && frm.idClientes.Count != 0)
+            {
+                List<int> idClientes = frm.idClientes;
+                List<Models.Clientes> clientes = new List<Models.Clientes>();
+
+                using (MABEntities db = new MABEntities())
+                {
+                    foreach(int id in idClientes)
+                    {
+                        clientes.Add(db.Clientes.Find(id));
+                    }
+
+                    ucDGVTabla.dataSource(clientes);
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                    "La busqueda fue cancelada o no se encontraron resultados",
+                    "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information
+                );
+            }
+        }
+
+        private void cerrarVentana(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+    }
+}
