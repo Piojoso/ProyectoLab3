5ba6c1f6a5bf021ea7c2ef4e79bbced475ee4ba7
Leonel Bongiovanni <labongiovanni@live.com.ar>
2020-04-14 12:45:18 -0300

    Varios cambios en frmReparaciones
    Primero se realizo un cambio de visual y ahora se utiliza el control
    de usuario ucDGVTabla para listar la informacion necesaria.
    
    Tambien se modifico la funcion que cargara la lista y se agrego el
    evento del boton Add del ucDGVTabla.
    
    Y finalmente se reorganizo el codigo y se eliminaron cosas innecesarias
    
    Aun hay muchos cambios mas por realizar.

--- MAB/Forms/Reparaciones/frmReparaciones.cs
+++ MAB/Forms/Reparaciones/frmReparaciones.cs
@@ -8,74 +8,135 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MAB.Models;
-using MAB;
+using MAB.Forms.Lavarropas;
 
 namespace MAB.Forms.CRUD.Reparaciones
 {
     public partial class frmReparaciones : Form
     {
-        public frmReparaciones()
+        public frmReparaciones(int? idLavarropas = null)
         {
+            /**
+             * TODO: Revisar el correcto funcionamiento
+             */
+
             InitializeComponent();
+            
+            cargarDGV(idLavarropas);
 
-            ucBG.Titulo = "Ultimas Reparaciones";
-
-            cargarDGV();
-
-            ucBG.Accion1 = "Agregar";
-            ucBG.Accion2 = "Modificar";
-            ucBG.Accion3 = "Ver Todos";
-
-            ucBG.evAccion1 += new EventHandler(btnAgregar);
-            ucBG.evAccion2 += new EventHandler(btnModificar);
-            ucBG.evAccion3 += verTodosReparacion;
+            ucDGVTabla.click_btnAdd += btnAgregar;
+            ucDGVTabla.click_btnModify += btnModificar;
+            ucDGVTabla.click_btnSearch += btnBuscar;
         }
 
-        private void cargarDGV()
+        private Models.Lavarropas lavarropas;
+
+        private void cargarDGV(int? idLavarropas)
         {
             /**
              * TODO: Revisar esta funcion... Parece que el || no funciona. Debo revisarlo bien
+             * --- HECHO: o mas bien, eliminado, ya que ahora muestra otra info.
              */
-            using (MABEntities db = new MABEntities())
+             
+            if(idLavarropas != null)
             {
-                //var data = from reparaciones in db.Reparaciones
-                //           where (((DateTime.Now - reparaciones.fechaIngreso).Days > 30) 
-                //           || reparaciones.estadoReparacion == estadosReparacion.EnCurso)
-                //           select reparaciones;
+                using (MABEntities db = new MABEntities())
+                {
+                    lavarropas = db.Lavarropas.Find(idLavarropas);
 
-                //ucBG.cargarDGV(data.ToList());
+                    var data = from reparaciones in db.Reparaciones
+                               where reparaciones.LavarropasId == lavarropas.Id
+                               select reparaciones;
+
+                    ucDGVTabla.dataSource(data.ToList());
+                }
+
+                Text = "Reparaciones del Lavarropas " + lavarropas.marca + lavarropas.modelo;
+            }
+            else
+            {
+                using (MABEntities db = new MABEntities())
+                {
+                    lavarropas = null;
+
+                    var shortList = from reparaciones in db.Reparaciones
+                               where reparaciones.estadoReparacion == estadosReparacion.EnCurso
+                               select reparaciones;
+
+                    ucDGVTabla.ShortListData = shortList.ToList();
+
+                    var fullList = db.Reparaciones;
+
+                    ucDGVTabla.FullListData = fullList.ToList();
+                    
+                }
+
+                Text = "Reparaciones";
             }
         }
 
+        #region Eventos de Botones
+
         private void btnAgregar(object sender, EventArgs e)
         {
-            frmAgregarReparaciones frm = new frmAgregarReparaciones();
-            frm.ShowDialog();
+            if (lavarropas != null)
+            {
+                frmAgregarReparaciones frm = new frmAgregarReparaciones(lavarropas.Id);
+                frm.ShowDialog();
+
+                cargarDGV(lavarropas.Id);
+            }
+            else
+            {
+                DialogResult resp = MessageBox.Show(
+                    "Se necesita un lavarropas al cual poder agregarle una Reparacion \n" +
+                    "Es por este motivo que a continuacion se abrira una ventana para que pueda seleccionar el \n" +
+                    "Lavarropas al que quiere agregarle una reparacion.",
+                    "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                frmSeleccionarLavarropas frmSeleccionarLavarropas = new frmSeleccionarLavarropas();
+                frmSeleccionarLavarropas.ShowDialog();
+
+                int idLavarropas = frmSeleccionarLavarropas.lavarropasSeleccionado;
+                
+                if(idLavarropas != -1)
+                {
+                    frmAgregarReparaciones frmAgregarReparaciones = new frmAgregarReparaciones(lavarropas.Id);
+                    frmAgregarReparaciones.ShowDialog();
+                }
+
+                cargarDGV(null);
+            }
         }
 
         private void btnModificar(object sender, EventArgs e)
         {
-            DataGridViewRow fila = ucBG.getSelectedItem();
+            /**
+             * TODO: Debo realizar varios cambios para luego volver aqui.
+             */
 
-            using (MABEntities db = new MABEntities())
-            {
-                /**
-                 * TODO: Revisar si tengo que castear value (yo me la juego que si)
-                 */
-                Models.Reparaciones reparacion = db.Reparaciones.Find(fila.Cells[0].Value);
+            //DataGridViewRow fila = ucBG.getSelectedItem();
 
-                /**
-                 * TODO: Tratar de forzar un reparacion == null. Solo para ver que ocurre aca.
-                 */
-                frmModificarReparacion frm = new frmModificarReparacion(reparacion.Id);
-                frm.ShowDialog();
-            }
+            //using (MABEntities db = new MABEntities())
+            //{
+            //    /**
+            //     * TODO: Revisar si tengo que castear value (yo me la juego que si)
+            //     */
+            //    Models.Reparaciones reparacion = db.Reparaciones.Find(fila.Cells[0].Value);
+
+            //    /**
+            //     * TODO: Tratar de forzar un reparacion == null. Solo para ver que ocurre aca.
+            //     */
+            //    frmModificarReparacion frm = new frmModificarReparacion(reparacion.Id);
+            //    frm.ShowDialog();
+            //}
         }
 
-        private void verTodosReparacion(object sender, EventArgs e)
+        private void btnBuscar(object sender, EventArgs e)
         {
-            frmVerTodosReparaciones frm = new frmVerTodosReparaciones();
-            frm.ShowDialog();
+
         }
+
+        #endregion
     }
 }
