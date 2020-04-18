04c5195c6946631b11ba34c8dc5a5e8fbddfcd12
Leonel Bongiovanni <labongiovanni@live.com.ar>
2020-04-18 16:53:32 -0300

    Agregado frmDetalleReparacion

---
+++ MAB/Forms/Reparaciones/frmDetalleReparacion.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace MAB.Forms.Reparaciones
+{
+    partial class frmDetalleReparacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucBottom = new ucLibrary.ucBottom();
+            this.ccLabel1 = new ccLibrary.ccLabel();
+            this.ccLabel2 = new ccLibrary.ccLabel();
+            this.ccLabel3 = new ccLibrary.ccLabel();
+            this.pnlBody = new System.Windows.Forms.Panel();
+            this.cclblTotal = new ccLibrary.ccLabel();
+            this.cclblTotalRepuestos = new ccLibrary.ccLabel();
+            this.cclblManoObra = new ccLibrary.ccLabel();
+            this.cclblMesesGarantia = new ccLibrary.ccLabel();
+            this.cclblReparacionRealizada = new ccLibrary.ccLabel();
+            this.cclblEstadoReparacion = new ccLibrary.ccLabel();
+            this.cclblErrorARepara = new ccLibrary.ccLabel();
+            this.cclblFechaEgreso = new ccLibrary.ccLabel();
+            this.cclblFechaIngreso = new ccLibrary.ccLabel();
+            this.cclblId = new ccLibrary.ccLabel();
+            this.btnVerLavarropas = new System.Windows.Forms.Button();
+            this.btnVerEntregas = new System.Windows.Forms.Button();
+            this.ccLabel11 = new ccLibrary.ccLabel();
+            this.ccLabel10 = new ccLibrary.ccLabel();
+            this.ccLabel9 = new ccLibrary.ccLabel();
+            this.ccLabel8 = new ccLibrary.ccLabel();
+            this.ccLabel7 = new ccLibrary.ccLabel();
+            this.ccLabel6 = new ccLibrary.ccLabel();
+            this.ccLabel5 = new ccLibrary.ccLabel();
+            this.ccLabel4 = new ccLibrary.ccLabel();
+            this.pnlBody.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ucBottom
+            // 
+            this.ucBottom.Accion1 = null;
+            this.ucBottom.Accion2 = null;
+            this.ucBottom.Accion3 = null;
+            this.ucBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ucBottom.Location = new System.Drawing.Point(0, 310);
+            this.ucBottom.MaximumSize = new System.Drawing.Size(3840, 82);
+            this.ucBottom.Name = "ucBottom";
+            this.ucBottom.NumButtons = 2;
+            this.ucBottom.Size = new System.Drawing.Size(747, 82);
+            this.ucBottom.TabIndex = 0;
+            // 
+            // ccLabel1
+            // 
+            this.ccLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel1.AutoSize = true;
+            this.ccLabel1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel1.Location = new System.Drawing.Point(43, 35);
+            this.ccLabel1.Name = "ccLabel1";
+            this.ccLabel1.Size = new System.Drawing.Size(22, 16);
+            this.ccLabel1.TabIndex = 1;
+            this.ccLabel1.Text = "Id:";
+            // 
+            // ccLabel2
+            // 
+            this.ccLabel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel2.AutoSize = true;
+            this.ccLabel2.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel2.Location = new System.Drawing.Point(218, 35);
+            this.ccLabel2.Name = "ccLabel2";
+            this.ccLabel2.Size = new System.Drawing.Size(111, 16);
+            this.ccLabel2.TabIndex = 2;
+            this.ccLabel2.Text = "Fecha de Ingreso:";
+            // 
+            // ccLabel3
+            // 
+            this.ccLabel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel3.AutoSize = true;
+            this.ccLabel3.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel3.Location = new System.Drawing.Point(499, 35);
+            this.ccLabel3.Name = "ccLabel3";
+            this.ccLabel3.Size = new System.Drawing.Size(108, 16);
+            this.ccLabel3.TabIndex = 3;
+            this.ccLabel3.Text = "Fecha de Egreso:";
+            // 
+            // pnlBody
+            // 
+            this.pnlBody.Controls.Add(this.cclblTotal);
+            this.pnlBody.Controls.Add(this.cclblTotalRepuestos);
+            this.pnlBody.Controls.Add(this.cclblManoObra);
+            this.pnlBody.Controls.Add(this.cclblMesesGarantia);
+            this.pnlBody.Controls.Add(this.cclblReparacionRealizada);
+            this.pnlBody.Controls.Add(this.cclblEstadoReparacion);
+            this.pnlBody.Controls.Add(this.cclblErrorARepara);
+            this.pnlBody.Controls.Add(this.cclblFechaEgreso);
+            this.pnlBody.Controls.Add(this.cclblFechaIngreso);
+            this.pnlBody.Controls.Add(this.cclblId);
+            this.pnlBody.Controls.Add(this.btnVerLavarropas);
+            this.pnlBody.Controls.Add(this.btnVerEntregas);
+            this.pnlBody.Controls.Add(this.ccLabel11);
+            this.pnlBody.Controls.Add(this.ccLabel10);
+            this.pnlBody.Controls.Add(this.ccLabel9);
+            this.pnlBody.Controls.Add(this.ccLabel8);
+            this.pnlBody.Controls.Add(this.ccLabel7);
+            this.pnlBody.Controls.Add(this.ccLabel6);
+            this.pnlBody.Controls.Add(this.ccLabel5);
+            this.pnlBody.Controls.Add(this.ccLabel4);
+            this.pnlBody.Controls.Add(this.ccLabel3);
+            this.pnlBody.Controls.Add(this.ccLabel1);
+            this.pnlBody.Controls.Add(this.ccLabel2);
+            this.pnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBody.Location = new System.Drawing.Point(0, 0);
+            this.pnlBody.Name = "pnlBody";
+            this.pnlBody.Size = new System.Drawing.Size(747, 310);
+            this.pnlBody.TabIndex = 4;
+            // 
+            // cclblTotal
+            // 
+            this.cclblTotal.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblTotal.AutoSize = true;
+            this.cclblTotal.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblTotal.Location = new System.Drawing.Point(482, 200);
+            this.cclblTotal.Name = "cclblTotal";
+            this.cclblTotal.Size = new System.Drawing.Size(0, 16);
+            this.cclblTotal.TabIndex = 23;
+            // 
+            // cclblTotalRepuestos
+            // 
+            this.cclblTotalRepuestos.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblTotalRepuestos.AutoSize = true;
+            this.cclblTotalRepuestos.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblTotalRepuestos.Location = new System.Drawing.Point(348, 200);
+            this.cclblTotalRepuestos.Name = "cclblTotalRepuestos";
+            this.cclblTotalRepuestos.Size = new System.Drawing.Size(0, 16);
+            this.cclblTotalRepuestos.TabIndex = 22;
+            // 
+            // cclblManoObra
+            // 
+            this.cclblManoObra.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblManoObra.AutoSize = true;
+            this.cclblManoObra.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblManoObra.Location = new System.Drawing.Point(147, 200);
+            this.cclblManoObra.Name = "cclblManoObra";
+            this.cclblManoObra.Size = new System.Drawing.Size(0, 16);
+            this.cclblManoObra.TabIndex = 21;
+            // 
+            // cclblMesesGarantia
+            // 
+            this.cclblMesesGarantia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblMesesGarantia.AutoSize = true;
+            this.cclblMesesGarantia.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblMesesGarantia.Location = new System.Drawing.Point(594, 134);
+            this.cclblMesesGarantia.Name = "cclblMesesGarantia";
+            this.cclblMesesGarantia.Size = new System.Drawing.Size(0, 16);
+            this.cclblMesesGarantia.TabIndex = 20;
+            // 
+            // cclblReparacionRealizada
+            // 
+            this.cclblReparacionRealizada.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblReparacionRealizada.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblReparacionRealizada.Location = new System.Drawing.Point(186, 134);
+            this.cclblReparacionRealizada.Name = "cclblReparacionRealizada";
+            this.cclblReparacionRealizada.Size = new System.Drawing.Size(275, 45);
+            this.cclblReparacionRealizada.TabIndex = 19;
+            // 
+            // cclblEstadoReparacion
+            // 
+            this.cclblEstadoReparacion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblEstadoReparacion.AutoSize = true;
+            this.cclblEstadoReparacion.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblEstadoReparacion.Location = new System.Drawing.Point(608, 79);
+            this.cclblEstadoReparacion.Name = "cclblEstadoReparacion";
+            this.cclblEstadoReparacion.Size = new System.Drawing.Size(0, 16);
+            this.cclblEstadoReparacion.TabIndex = 18;
+            // 
+            // cclblErrorARepara
+            // 
+            this.cclblErrorARepara.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblErrorARepara.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblErrorARepara.Location = new System.Drawing.Point(153, 79);
+            this.cclblErrorARepara.Name = "cclblErrorARepara";
+            this.cclblErrorARepara.Size = new System.Drawing.Size(308, 45);
+            this.cclblErrorARepara.TabIndex = 17;
+            // 
+            // cclblFechaEgreso
+            // 
+            this.cclblFechaEgreso.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblFechaEgreso.AutoSize = true;
+            this.cclblFechaEgreso.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblFechaEgreso.Location = new System.Drawing.Point(613, 35);
+            this.cclblFechaEgreso.Name = "cclblFechaEgreso";
+            this.cclblFechaEgreso.Size = new System.Drawing.Size(0, 16);
+            this.cclblFechaEgreso.TabIndex = 16;
+            // 
+            // cclblFechaIngreso
+            // 
+            this.cclblFechaIngreso.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblFechaIngreso.AutoSize = true;
+            this.cclblFechaIngreso.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblFechaIngreso.Location = new System.Drawing.Point(335, 35);
+            this.cclblFechaIngreso.Name = "cclblFechaIngreso";
+            this.cclblFechaIngreso.Size = new System.Drawing.Size(0, 16);
+            this.cclblFechaIngreso.TabIndex = 15;
+            // 
+            // cclblId
+            // 
+            this.cclblId.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cclblId.AutoSize = true;
+            this.cclblId.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.cclblId.Location = new System.Drawing.Point(71, 35);
+            this.cclblId.Name = "cclblId";
+            this.cclblId.Size = new System.Drawing.Size(0, 16);
+            this.cclblId.TabIndex = 14;
+            // 
+            // btnVerLavarropas
+            // 
+            this.btnVerLavarropas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVerLavarropas.Location = new System.Drawing.Point(416, 252);
+            this.btnVerLavarropas.Name = "btnVerLavarropas";
+            this.btnVerLavarropas.Size = new System.Drawing.Size(93, 23);
+            this.btnVerLavarropas.TabIndex = 13;
+            this.btnVerLavarropas.Text = "Ver Lavarropas";
+            this.btnVerLavarropas.UseVisualStyleBackColor = true;
+            this.btnVerLavarropas.Click += new System.EventHandler(this.btnVerLavarropas_Click);
+            // 
+            // btnVerEntregas
+            // 
+            this.btnVerEntregas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVerEntregas.Location = new System.Drawing.Point(597, 193);
+            this.btnVerEntregas.Name = "btnVerEntregas";
+            this.btnVerEntregas.Size = new System.Drawing.Size(93, 23);
+            this.btnVerEntregas.TabIndex = 12;
+            this.btnVerEntregas.Text = "Ver Entregas";
+            this.btnVerEntregas.UseVisualStyleBackColor = true;
+            this.btnVerEntregas.Click += new System.EventHandler(this.btnVerEntregas_Click);
+            // 
+            // ccLabel11
+            // 
+            this.ccLabel11.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel11.AutoSize = true;
+            this.ccLabel11.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel11.Location = new System.Drawing.Point(43, 134);
+            this.ccLabel11.Name = "ccLabel11";
+            this.ccLabel11.Size = new System.Drawing.Size(137, 16);
+            this.ccLabel11.TabIndex = 11;
+            this.ccLabel11.Text = "Reparacion Realizada:";
+            // 
+            // ccLabel10
+            // 
+            this.ccLabel10.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel10.AutoSize = true;
+            this.ccLabel10.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel10.Location = new System.Drawing.Point(238, 256);
+            this.ccLabel10.Name = "ccLabel10";
+            this.ccLabel10.Size = new System.Drawing.Size(76, 16);
+            this.ccLabel10.TabIndex = 10;
+            this.ccLabel10.Text = "Lavarropas:";
+            // 
+            // ccLabel9
+            // 
+            this.ccLabel9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel9.AutoSize = true;
+            this.ccLabel9.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel9.Location = new System.Drawing.Point(436, 200);
+            this.ccLabel9.Name = "ccLabel9";
+            this.ccLabel9.Size = new System.Drawing.Size(40, 16);
+            this.ccLabel9.TabIndex = 9;
+            this.ccLabel9.Text = "Total:";
+            // 
+            // ccLabel8
+            // 
+            this.ccLabel8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel8.AutoSize = true;
+            this.ccLabel8.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel8.Location = new System.Drawing.Point(218, 200);
+            this.ccLabel8.Name = "ccLabel8";
+            this.ccLabel8.Size = new System.Drawing.Size(124, 16);
+            this.ccLabel8.TabIndex = 8;
+            this.ccLabel8.Text = "Total de Repuestos:";
+            // 
+            // ccLabel7
+            // 
+            this.ccLabel7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel7.AutoSize = true;
+            this.ccLabel7.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel7.Location = new System.Drawing.Point(43, 200);
+            this.ccLabel7.Name = "ccLabel7";
+            this.ccLabel7.Size = new System.Drawing.Size(98, 16);
+            this.ccLabel7.TabIndex = 7;
+            this.ccLabel7.Text = "Mano de Obra:";
+            // 
+            // ccLabel6
+            // 
+            this.ccLabel6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel6.AutoSize = true;
+            this.ccLabel6.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel6.Location = new System.Drawing.Point(467, 79);
+            this.ccLabel6.Name = "ccLabel6";
+            this.ccLabel6.Size = new System.Drawing.Size(140, 16);
+            this.ccLabel6.TabIndex = 6;
+            this.ccLabel6.Text = "Estado de Reparacion:";
+            // 
+            // ccLabel5
+            // 
+            this.ccLabel5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel5.AutoSize = true;
+            this.ccLabel5.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel5.Location = new System.Drawing.Point(467, 134);
+            this.ccLabel5.Name = "ccLabel5";
+            this.ccLabel5.Size = new System.Drawing.Size(121, 16);
+            this.ccLabel5.TabIndex = 5;
+            this.ccLabel5.Text = "Meses de Garantia:";
+            // 
+            // ccLabel4
+            // 
+            this.ccLabel4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ccLabel4.AutoSize = true;
+            this.ccLabel4.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F);
+            this.ccLabel4.Location = new System.Drawing.Point(43, 79);
+            this.ccLabel4.Name = "ccLabel4";
+            this.ccLabel4.Size = new System.Drawing.Size(104, 16);
+            this.ccLabel4.TabIndex = 4;
+            this.ccLabel4.Text = "Error A Reparar:";
+            // 
+            // frmDetalleReparacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 392);
+            this.Controls.Add(this.pnlBody);
+            this.Controls.Add(this.ucBottom);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmDetalleReparacion";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.pnlBody.ResumeLayout(false);
+            this.pnlBody.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ucLibrary.ucBottom ucBottom;
+        private ccLibrary.ccLabel ccLabel1;
+        private ccLibrary.ccLabel ccLabel2;
+        private ccLibrary.ccLabel ccLabel3;
+        private System.Windows.Forms.Panel pnlBody;
+        private ccLibrary.ccLabel ccLabel10;
+        private ccLibrary.ccLabel ccLabel9;
+        private ccLibrary.ccLabel ccLabel8;
+        private ccLibrary.ccLabel ccLabel7;
+        private ccLibrary.ccLabel ccLabel6;
+        private ccLibrary.ccLabel ccLabel5;
+        private ccLibrary.ccLabel ccLabel4;
+        private System.Windows.Forms.Button btnVerLavarropas;
+        private System.Windows.Forms.Button btnVerEntregas;
+        private ccLibrary.ccLabel ccLabel11;
+        private ccLibrary.ccLabel cclblTotal;
+        private ccLibrary.ccLabel cclblTotalRepuestos;
+        private ccLibrary.ccLabel cclblManoObra;
+        private ccLibrary.ccLabel cclblMesesGarantia;
+        private ccLibrary.ccLabel cclblReparacionRealizada;
+        private ccLibrary.ccLabel cclblEstadoReparacion;
+        private ccLibrary.ccLabel cclblErrorARepara;
+        private ccLibrary.ccLabel cclblFechaEgreso;
+        private ccLibrary.ccLabel cclblFechaIngreso;
+        private ccLibrary.ccLabel cclblId;
+    }
+}
---
+++ MAB/Forms/Reparaciones/frmDetalleReparacion.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MAB.Models;
+using MAB.Forms.CRUD.Reparaciones;
+using MAB.Forms.Lavarropas;
+using MAB.Forms.Entregas;
+
+namespace MAB.Forms.Reparaciones
+{
+    public partial class frmDetalleReparacion : Form
+    {
+        public frmDetalleReparacion(int idReparacion)
+        {
+            InitializeComponent();
+
+            cargarReparacion(idReparacion);
+
+            ucBottom.Accion1 = "Modificar";
+            ucBottom.Accion2 = "Cerrar";
+
+            ucBottom.evAccion1 += modificarReparacion;
+            ucBottom.evAccion2 += cerrarVentana;
+        }
+
+        Models.Reparaciones reparacion;
+
+        private void cargarReparacion(int idReparacion)
+        {
+            using (MABEntities db = new MABEntities())
+            {
+                reparacion = db.Reparaciones.Find(idReparacion);
+            }
+
+            cclblId.Text = reparacion.Id.ToString();
+            cclblFechaIngreso.Text = reparacion.fechaIngreso.ToShortDateString();
+            cclblErrorARepara.Text = reparacion.errorAReparar;
+            cclblEstadoReparacion.Text = reparacion.estadoReparacion.ToString();
+            cclblReparacionRealizada.Text = reparacion.reparacionRealizada;
+            cclblMesesGarantia.Text = reparacion.mesesGarantia == null? "0" : reparacion.mesesGarantia.ToString();
+            cclblManoObra.Text = reparacion.manoDeObra.ToString();
+            cclblTotalRepuestos.Text = reparacion.totalRepuestos.ToString();
+            cclblTotal.Text = Convert.ToString(reparacion.manoDeObra + reparacion.totalRepuestos);
+
+            Text = "Detalle de la Reparacion: " + reparacion.Id;
+        }
+
+        #region eventos de ucBottom
+
+        private void modificarReparacion(object sender, EventArgs e)
+        {
+            frmModificarReparacion frm = new frmModificarReparacion(reparacion.Id);
+            frm.ShowDialog();
+
+            cargarReparacion(reparacion.Id);
+        }
+
+        private void cerrarVentana(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        #endregion
+
+        #region eventos de botones
+
+        private void btnVerEntregas_Click(object sender, EventArgs e)
+        {
+            frmEntregas frm = new frmEntregas(reparacion.Id);
+            frm.ShowDialog();
+        }
+
+        private void btnVerLavarropas_Click(object sender, EventArgs e)
+        {
+            frmDetalleLavarropas frm = new frmDetalleLavarropas(reparacion.Lavarropas.Id);
+            frm.ShowDialog();
+        }
+
+        #endregion
+    }
+}
