9809b0aa6c10e16693ed7aa6dcdbd6c544cb7eab
Leonel Bongiovanni <labongiovanni@live.com.ar>
2020-05-13 19:19:40 -0300

    Agregado frmRepuestos
    Este frm sera el encargado de enseñar el stock de repuestos. Pudiendo
    desde aqui facilmente acceder a un frm y poder agregar un nuevo
    repuesto, o modificar uno existente. Modificarle el precio o el stock
    existente.
    
    Tambien desde aqui se podran ver todos los repuestos usado en una
    reparacion.

---
+++ MAB/Forms/Repuestos/frmRepuestos.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MAB.Forms.Repuestos
+{
+    partial class frmRepuestos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucDGVTabla = new ucLibrary.ucDGVTabla();
+            this.SuspendLayout();
+            // 
+            // ucDGVTabla
+            // 
+            this.ucDGVTabla.buttonAdd = System.Drawing.Color.ForestGreen;
+            this.ucDGVTabla.buttonDelete = System.Drawing.Color.IndianRed;
+            this.ucDGVTabla.buttonModify = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ucDGVTabla.buttonSearch = System.Drawing.Color.SteelBlue;
+            this.ucDGVTabla.buttonSeeAll = System.Drawing.Color.DarkGray;
+            this.ucDGVTabla.cargarCMS = null;
+            this.ucDGVTabla.ColumnHeaderStyle = null;
+            this.ucDGVTabla.Columns = null;
+            this.ucDGVTabla.DeleteVisibility = false;
+            this.ucDGVTabla.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucDGVTabla.FondoBotones = System.Drawing.Color.Teal;
+            this.ucDGVTabla.FullListData = null;
+            this.ucDGVTabla.Location = new System.Drawing.Point(0, 0);
+            this.ucDGVTabla.Name = "ucDGVTabla";
+            this.ucDGVTabla.RowsCellStyle = null;
+            this.ucDGVTabla.RowsHeaderStyle = null;
+            this.ucDGVTabla.ShortListData = null;
+            this.ucDGVTabla.Size = new System.Drawing.Size(918, 466);
+            this.ucDGVTabla.TabIndex = 0;
+            // 
+            // frmRepuestos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 466);
+            this.Controls.Add(this.ucDGVTabla);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmRepuestos";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ucLibrary.ucDGVTabla ucDGVTabla;
+    }
+}
---
+++ MAB/Forms/Repuestos/frmRepuestos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MAB.Models;
+
+namespace MAB.Forms.Repuestos
+{
+    public partial class frmRepuestos : Form
+    {
+        public frmRepuestos()
+        {
+            InitializeComponent();
+
+            cargarDGV();
+
+            ucDGVTabla.click_btnAdd += btnAgregar;
+            ucDGVTabla.click_btnModify += btnModificar;
+            ucDGVTabla.click_btnSearch += btnBuscar;
+        }
+
+        private void cargarDGV()
+        {
+            using (MABEntities db = new MABEntities())
+            {
+                var repuestos = db.Repuestos;
+
+                ucDGVTabla.FullListData = repuestos.ToList();
+            }
+        }
+
+        #region eventos de botones
+
+        private void btnAgregar(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModificar(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
